ca088be73f103b84ff702c0c68f18718f18751a9
Marc (VPS) <marc.jimenez.gimeno@gmail.com>
2023-10-18 15:46:57 +0000

    [Kata2] Iteration 2

--- 02-coffee-machine/src/coffee_machine.js
+++ 02-coffee-machine/src/coffee_machine.js
@@ -4,19 +4,31 @@ class CoffeeMachine {
     sweetness
     stick
 
+    balance
+
     order
 
     drinkMaker = new DrinkMaker()
 
-    offeredDrinks = ["coffee", "tea", "chocolate"]
+    offeredDrinks = [
+        { name: "coffee", pricing: 60 },
+        { name: "tea", pricing: 40 },
+        { name: "chocolate", pricing: 50 }
+    ]
     offeredSweetness = [0, 1, 2]
 
     constructor() {
         this.sweetness = 0
+        this.balance = 0
+    }
+
+    getDrinkData(_drink) {
+        const drink = _drink ? _drink : this.drink
+        return this.offeredDrinks.find(offeredDrink => offeredDrink.name == drink)
     }
 
     setDrink(drink) {
-        const isValid = this.offeredDrinks.includes(drink)
+        const isValid = this.getDrinkData(drink)
         if (isValid) {
             this.drink = drink
         }
@@ -34,8 +46,31 @@ class CoffeeMachine {
         return this.sweetness != 0
     }
 
+    insertMoney(cts) {
+        if (!Number.isInteger(cts)) throw "Inserted X not"
+
+        this.balance = this.balance + cts
+    }
+
+    checkBalance() {
+        const drinkWithPrice = this.getDrinkData(this.drink)
+        const remainingBalance = this.balance - drinkWithPrice.pricing
+        return remainingBalance
+    }
+
+    isBalanceEnough() {
+        const balance = this.checkBalance()
+        return balance >= 0
+    }
+
     parseOrder() {
         let command = ""
+
+        if (!this.isBalanceEnough()) {
+            const missingBalance = this.checkBalance() * -1
+            command = "M:Missing " + missingBalance + "cts to purchase " + this.drink
+            return command
+        }
 
         const drinks = {
             "coffee": "C",
--- 02-coffee-machine/test/coffee_machine.test.js
+++ 02-coffee-machine/test/coffee_machine.test.js
@@ -2,6 +2,8 @@ let CoffeeMachine = require('../src/coffee_machine')
 
 let coffeeMachine
 
+const buyWhatever = 1000
+
 beforeEach(() => {
 	coffeeMachine = new CoffeeMachine()
 })
@@ -10,6 +12,10 @@ describe('CoffeeMachine', function () {
 	describe("consturctor", () => {
 		test("sweetness level default should be 0", () => {
 			expect(coffeeMachine.sweetness).toBe(0)
+		})
+
+		test("balance should initially be 0", () => {
+			expect(coffeeMachine.balance).toBe(0)
 		})
 	})
 
@@ -57,7 +63,8 @@ describe('CoffeeMachine', function () {
 	})
 
 	describe("method parseOrder should", () => {
-		test("correctly parse drink with no sweetness to drink maker command", () => {
+		test("correctly parse drink with no sweetness to drink maker command and balance is enough", () => {
+			coffeeMachine.insertMoney(buyWhatever)
 			const sweetness = 0
 			coffeeMachine.setSweetness(sweetness)
 			const drink = "coffee"
@@ -66,7 +73,8 @@ describe('CoffeeMachine', function () {
 			expect(command).toBe("C::")
 		})
 
-		test("correctly parse drink with sweetness to drink maker command", () => {
+		test("correctly parse drink with sweetness to drink maker command and balance is enough", () => {
+			coffeeMachine.insertMoney(buyWhatever)
 			const sweetness = 2
 			coffeeMachine.setSweetness(sweetness)
 			const drink = "coffee"
@@ -80,5 +88,79 @@ describe('CoffeeMachine', function () {
 			coffeeMachine.setDrink(invalid_drink)
 			expect(() => coffeeMachine.parseOrder()).toThrow()
 		})
+
+		test("return a message with missing amount when balance is not enough", () => {
+			coffeeMachine.setDrink("coffee")
+			coffeeMachine.insertMoney(40)
+			const message = coffeeMachine.parseOrder()
+			const expectedMessage = "M:Missing 20cts to purchase coffee"
+			expect(message).toBe(expectedMessage)
+		})
+
+		test("not return a message when order is processed correctly", () => {
+			coffeeMachine.insertMoney(buyWhatever)
+			const drink = "coffee"
+			coffeeMachine.setDrink(drink)
+			const command = coffeeMachine.parseOrder()
+			expect(command[0]).not.toBe("M")
+		})
 	})
-});
+
+	describe("method insertMoney should", () => {
+		test("increase balance for the inserted amount", () => {
+			const insertedAmount = 100
+			coffeeMachine.insertMoney(insertedAmount)
+			expect(coffeeMachine.balance).toBe(insertedAmount)
+		})
+
+		test("increase balance for the inserted amount when it already had balance", () => {
+			const initialBalance = 20
+			coffeeMachine.balance = initialBalance
+			const insertedAmount = 100
+			coffeeMachine.insertMoney(insertedAmount)
+			expect(coffeeMachine.balance).toBe(insertedAmount + initialBalance)
+		})
+
+		test.todo("to throw error when inserted coin is not valid")
+
+		test("throw error when inserted incorrectly", () => {
+			const insertedAmount = "paco"
+			expect(() => coffeeMachine.insertMoney(insertedAmount)).toThrow()
+		})
+	})
+
+	describe("method checkBalance should return the difference between the balance and the drink price", () => {
+		const data = [
+			{ testName: "when balance is above 0", insertedMoney: 100, expectedBalance: 40 },
+			{ testName: "when balance is 0", insertedMoney: 60, expectedBalance: 0 },
+			{ testName: "when balance is below 0", insertedMoney: 40, expectedBalance: -20 },
+		]
+
+		data.forEach(d => {
+			test(d.testName, () => {
+				coffeeMachine.setDrink("coffee")
+				coffeeMachine.insertMoney(d.insertedMoney)
+				expect(coffeeMachine.checkBalance()).toBe(d.expectedBalance)
+			})
+		})
+	})
+
+	describe("method isBalanceEnough should", () => {
+		test("return true when balance after purchase would be 0 or above", () => {
+			coffeeMachine.setDrink("coffee")
+			coffeeMachine.insertMoney(100)
+			expect(coffeeMachine.isBalanceEnough()).toBe(true)
+		})
+		test("return false when balance after purchase would be below 0", () => {
+			coffeeMachine.setDrink("coffee")
+			coffeeMachine.insertMoney(40)
+			expect(coffeeMachine.isBalanceEnough()).toBe(false)
+		})
+	})
+})
+
+/*
+- The drink maker should make the drinks only if the correct amount of money is given
+- If not enough money is provided, we want to send a message to the drink maker. The message should contains at least the amount of money missing.
+
+*/
